40b2921e48535fb683cf0d9fd4c64532b9e22830
Oliver Killane <oliverkillane@gmail.com>
2022-11-16 20:54:02 +0000

    Fixed issue with cabal clean, and removed stb_image header files from backend/src
    Ethan Range <65268454+ethanrange@users.noreply.github.com>

--- Setup.hs
+++ Setup.hs
@@ -1,13 +1,4 @@
-module Main (main) where
-
-import Control.Monad
-  ( unless,
-    when,
-  )
-import Data.Maybe
-  ( fromJust,
-    fromMaybe,
-  )
+import Data.Maybe (fromJust)
 import Distribution.PackageDescription
 import Distribution.Simple
 import Distribution.Simple.LocalBuildInfo
@@ -20,21 +11,13 @@ main =
   defaultMainWithHooks
     simpleUserHooks
       { preConf = buildBackend,
-        confHook = addExtraLibs,
-        postClean = cmakeClean
+        confHook = addExtraLibs
       }
 
 buildBackend :: Args -> ConfigFlags -> IO HookedBuildInfo
 buildBackend _ _ = do
   readProcess "make" ["--directory=backend"] "" >>= putStrLn
   return emptyHookedBuildInfo
-
-{- See https://github.com/haskell/cabal/issues/6112
-`cabal clean/new-clean/v2-clean does not consider the extra tmp files, and 
-hence no hooks are run, this is a bug with cabal. -}
-cmakeClean :: Args -> CleanFlags -> PackageDescription -> () -> IO ()
-cmakeClean _ _ _ _ =
-    readProcess "make" ["clean", "--directory=backend"] "" >>= putStrLn
 
 addExtraLibs ::
   (GenericPackageDescription, HookedBuildInfo) ->
@@ -54,7 +37,7 @@ addExtraLibs (description, buildInfo) flags = do
                 Just $ lib
                     { libBuildInfo =
                         libraryBuildInfo
-                          { extraLibDirs = (dir ++ "/backend/libs") : extraLibDirs libraryBuildInfo
+                          { extraLibDirs = (dir ++ "/dist-newstyle/backend-libs") : extraLibDirs libraryBuildInfo
                           }
                     }
             }
--- setup.sh
+++
@@ -1,4 +0,0 @@
-sudo cp -r external/glad /usr/include/ ; echo "Moved glad to include"
-sudo cp -r external/KHR /usr/include/ ; echo "Moved KHR to include"
-
-cp external/glad.c src/ ; echo "Installed glad to source"
